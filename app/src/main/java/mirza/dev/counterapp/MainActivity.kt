e15dd8642d9d299ead3c9d804e07ad756ad08af1
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-12-26 05:04:10 +0600

    feat(main activity): increase_numbers_and _show_toast_message

--- app/src/main/java/mirza/dev/counterapp/MainActivity.kt
+++ app/src/main/java/mirza/dev/counterapp/MainActivity.kt
@@ -2,10 +2,28 @@ package mirza.dev.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO: declare variable
+        var counter = 0
+
+        // TODO: TextView
+        val countTextView = findViewById<TextView>(R.id.countTextView)
+
+        // TODO: Button
+        val clickMeButton = findViewById<Button>(R.id.clickMeButton)
+        clickMeButton.setOnClickListener {
+            "Count: ${counter++}".also { countTextView.text = it }
+
+            // TODO: Toast Message
+            Toast.makeText(this, "Number is increasing", Toast.LENGTH_LONG).show()
+        }
     }
 }
